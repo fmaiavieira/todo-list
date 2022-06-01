b15d9eb57dfe4ac9da51fc4cdab889d6a88057ab
Felipe Maia Vieira <fmaiavieira@gmail.com>
2022-05-31 22:01:28 -0300

    Todo-list: animate task expand

--- todo-list/src/app/board/task-list/task-item/task-edit-form/task-edit-form.component.ts
+++ todo-list/src/app/board/task-list/task-item/task-edit-form/task-edit-form.component.ts
@@ -1,14 +1,41 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { BoardTask, TaskFormGroup } from '../../../board';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-task-edit-form',
   templateUrl: './task-edit-form.component.html',
   styleUrls: ['./task-edit-form.component.scss'],
+  animations: [
+    trigger('openClose', [
+      // ...
+      state(
+        'opened',
+        style({
+          minHeight: '315px',
+        })
+      ),
+      state(
+        'closed',
+        style({
+          height: '0px',
+        })
+      ),
+      transition('opened => closed', [animate('0.1s')]),
+      transition('closed => opened', [animate('0.1s')]),
+    ]),
+  ],
 })
 export class TaskEditFormComponent implements OnInit {
   @Input() task!: BoardTask;
+  @Input() openClose!: Boolean;
   @Output() closeForm = new EventEmitter();
   @Output() onEditTask = new EventEmitter<BoardTask>();
 
@@ -19,12 +46,13 @@ export class TaskEditFormComponent implements OnInit {
     position: [0],
     status: ['todo'],
     name: [''],
-    expire_date: [''],
+    expire_date: [new Date()],
     description: [''],
   }) as TaskFormGroup;
 
   ngOnInit() {
     this.taskForm.patchValue(this.task);
+    console.log(this.taskForm.value);
   }
 
   saveTask() {
